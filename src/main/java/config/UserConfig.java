45cea95ac50408210851c8522b0db3bb90430d1c
Z0R7N <dmtrii.a@gmail.com>
2021-04-23 21:29:06 +0300

    Initial commit

---
+++ src/main/java/config/UserConfig.java
@@ -0,0 +1,7 @@
+package config;
+
+public class UserConfig {
+    public static final String name = "Tim Andersen";
+    public static final String email = "tim@company.pl";
+    public static final String phone = "600-123-100";
+}
